951f361f4e4c0d727482c91d87ccf246d073f5e1
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-12-18 11:05:42 +0100

    klar med funktion divide

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 let tal1x = document.querySelector("#tal1");
 let tal2x = document.querySelector("#tal2");
 
-
-function lös(){
-    document.getElementById("result").value = result;
-}
-
 function plus(){
     let tal1_värde = parseInt( tal1x.value);
     let tal2_värde = parseInt( tal2x.value);
@@ -30,6 +25,15 @@ function multiply(){
     let tal2_värde = parseInt( tal2x.value);
 
     let result = tal1_värde*tal2_värde;
+    document.getElementById("result").value = result;
+
+}
+
+function divide(){
+    let tal1_värde = parseInt( tal1x.value);
+    let tal2_värde = parseInt( tal2x.value);
+
+    let result = tal1_värde/tal2_värde;
     document.getElementById("result").value = result;
 
 }
