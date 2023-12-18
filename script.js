865531474a3caf104e2bad6fd5439ad83fbe2295
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-12-18 11:03:45 +0100

    plus funktionen funkar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let tal1x = document.querySelector("#tal1");
+let tal2x = document.querySelector("#tal2");
+
+
+function lös(){
+    document.getElementById("result").value = result;
+}
+
+function plus(){
+    console.log("plus:"+tal1x.value);
+
+    let tal1_värde = parseInt( tal1x.value);
+    let tal2_värde = parseInt( tal2x.value);
+
+    let result = tal1_värde+tal2_värde;
+    document.getElementById("result").value = result;
+
+}
+
+
